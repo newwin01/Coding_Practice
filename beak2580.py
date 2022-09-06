6e69a98d757fda2de4c597f34499d72c626380ab
newwin01 <newwin0198@handong.ac.kr>
2022-09-06 21:57:04 +0900

    beak2580 solve

--- beak2580.py
+++ beak2580.py
@@ -1,7 +1,11 @@
 #map 생성, 0 찾기
-sudoku_map = [list(map(int, input().split())) for _ in range(9)]
+import numbers
 
+
+sudoku_map = [list(map(int, input().split())) for _ in range(9)]
 zeros = [(i,j) for i in range(9) for j in range(9) if sudoku_map[i][j]==0]
+
+
 #넣을 수 있는 숫자 찾기
 def exist(i,j):
     number = [1,2,3,4,5,6,7,8,9]
@@ -22,8 +26,27 @@ def exist(i,j):
         for m in range(j*3,(j+1)*3):
             if sudoku_map[l][m] in number:
                 number.remove(sudoku_map[l][m])
-    
-
     return number
 
+printed = False;
 
+def solution(n):
+    global printed
+    if n == len(zeros):
+        for pr in sudoku_map:
+            print(*pr)
+            printed = True;
+        return
+
+    else:
+        (i,j) = zeros[n]
+        number = exist(i,j)
+
+        for x in number:
+            sudoku_map[i][j] = x
+            solution(n+1)
+            sudoku_map[i][j] = 0
+
+
+
+solution(0)
